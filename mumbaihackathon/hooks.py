5929b35202a18f147507eb6b29d25cd08811cec1
Valmik Jangla <mail@valmik.in>
2016-02-04 17:52:16 +0530

    Inital Commit

---
+++ mumbaihackathon/hooks.py
@@ -0,0 +1,113 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+app_name = "mumbaihackathon"
+app_title = "Mumbai Hackathon"
+app_publisher = "Frappe Technologies"
+app_description = "Website for Mumbai Hackathon"
+app_icon = "octicon octicon-file-directory"
+app_color = "grey"
+app_email = "hello@frappe.io"
+app_version = "0.0.1"
+app_license = "MIT"
+
+# Includes in <head>
+# ------------------
+
+# include js, css files in header of desk.html
+# app_include_css = "/assets/mumbaihackathon/css/mumbaihackathon.css"
+# app_include_js = "/assets/mumbaihackathon/js/mumbaihackathon.js"
+
+# include js, css files in header of web template
+# web_include_css = "/assets/mumbaihackathon/css/mumbaihackathon.css"
+# web_include_js = "/assets/mumbaihackathon/js/mumbaihackathon.js"
+
+# Home Pages
+# ----------
+
+# application home page (will override Website Settings)
+# home_page = "login"
+
+# website user home page (by Role)
+# role_home_page = {
+#	"Role": "home_page"
+# }
+
+# Website user home page (by function)
+# get_website_user_home_page = "mumbaihackathon.utils.get_home_page"
+
+# Generators
+# ----------
+
+# automatically create page for each record of this doctype
+# website_generators = ["Web Page"]
+
+# Installation
+# ------------
+
+# before_install = "mumbaihackathon.install.before_install"
+# after_install = "mumbaihackathon.install.after_install"
+
+# Desk Notifications
+# ------------------
+# See frappe.core.notifications.get_notification_config
+
+# notification_config = "mumbaihackathon.notifications.get_notification_config"
+
+# Permissions
+# -----------
+# Permissions evaluated in scripted ways
+
+# permission_query_conditions = {
+# 	"Event": "frappe.desk.doctype.event.event.get_permission_query_conditions",
+# }
+#
+# has_permission = {
+# 	"Event": "frappe.desk.doctype.event.event.has_permission",
+# }
+
+# Document Events
+# ---------------
+# Hook on document methods and events
+
+# doc_events = {
+# 	"*": {
+# 		"on_update": "method",
+# 		"on_cancel": "method",
+# 		"on_trash": "method"
+#	}
+# }
+
+# Scheduled Tasks
+# ---------------
+
+# scheduler_events = {
+# 	"all": [
+# 		"mumbaihackathon.tasks.all"
+# 	],
+# 	"daily": [
+# 		"mumbaihackathon.tasks.daily"
+# 	],
+# 	"hourly": [
+# 		"mumbaihackathon.tasks.hourly"
+# 	],
+# 	"weekly": [
+# 		"mumbaihackathon.tasks.weekly"
+# 	]
+# 	"monthly": [
+# 		"mumbaihackathon.tasks.monthly"
+# 	]
+# }
+
+# Testing
+# -------
+
+# before_tests = "mumbaihackathon.install.before_tests"
+
+# Overriding Whitelisted Methods
+# ------------------------------
+#
+# override_whitelisted_methods = {
+# 	"frappe.desk.doctype.event.event.get_events": "mumbaihackathon.event.get_events"
+# }
+
